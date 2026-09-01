8093fd2ef9b8707c7997351c58dba4218feee79c
Surhandeedar <s.urhandeedar.9@gmail.com>
2026-09-01 21:35:20 +0500

    Add C source files for Task 6 and Task 7

---
+++ task7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	int age;
+	float height;
+	printf("Enter your age and height: ");
+	scanf("%i\n %f", &age, &height);
+	printf("You are %i years old and %.2f meters tall.\n", age, height);
+	return 0;
+}
