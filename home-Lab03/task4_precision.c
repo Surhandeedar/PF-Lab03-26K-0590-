81aff8ed72b3cf3f7f2fa71fdf4ff0165cbbea97
Surhandeedar <s.urhandeedar.9@gmail.com>
2026-09-07 20:48:27 +0500

    Add precision printing of pi in task4_precision.c

---
+++ home-Lab03/task4_precision.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	double pi=3.14159265358979;
+	printf("pi: %.2lf\n", pi);
+	printf("pi: %.4lf\n", pi);
+	printf("pi: %.6lf\n", pi);
+	printf("pi: %.10lf\n", pi);
+	return 0;
+}
