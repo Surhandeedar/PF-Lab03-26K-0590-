ba67e9b84234eaac2826e988babb4a76fbce387c
Surhandeedar <s.urhandeedar.9@gmail.com>
2026-09-07 20:46:10 +0500

    Add task2_specifiers.c for student information display

---
+++ home-Lab03/task2_specifiers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	char Student_Name[]="Surhan";
+	int ID=26;
+	char Grade='A';
+	float GPA=89.35;
+	long long Code=823542123967L;
+	
+	printf("Student Name: %s, ID: %d, Grade: %c, GPA: %.2f, Code: %lld", Student_Name, ID, Grade, GPA, Code);
+	return 0;
+}
