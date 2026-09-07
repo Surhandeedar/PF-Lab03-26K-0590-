f536f997c390dfe4b74a94aa940963dedb639701
Surhandeedar <s.urhandeedar.9@gmail.com>
2026-09-07 20:47:31 +0500

    Add student ID card display program

---
+++ home-Lab03/task3_idcard.c
@@ -0,0 +1,8 @@
+ #include<stdio.h>
+ int main() {
+ 	printf("\a\"STUDENT ID CARD\"\n");
+ 	printf("Name:\t\tSurhan\n");
+ 	printf("Roll No:\t26K-0590\n");
+ 	printf("Department:\t\FAST\CS\n");
+ 	return 0;
+ }
